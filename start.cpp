e64f7a2e504aa0b2036bad62ffca09bbba651fbd
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2019-02-27 18:50:31 +0900

    First push!!

---
+++ start.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+template <typename T = int>
+T in();
+
+int main() {
+  
+    return 0;
+}
+
+template <typename T = int>
+T in() {
+    T temp;
+    cin >> temp;
+    return temp;
+}
+
