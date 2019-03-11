70a0eeeca9cccecd71dc6f062bab8da05c8d0caf
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2019-03-11 21:19:44 +0900

    とりあえずクラスに処理を退避。

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ T in() {
     cin >> temp;
     return temp;
 }
-
+/*
 uint64_t GetXor (uint64_t A, uint64_t B) {
     uint64_t ret;
     for (uint64_t i = A; i <=B; i++) {
@@ -21,39 +21,61 @@ uint64_t GetXor (uint64_t A, uint64_t B) {
     }
     return ret;
 }
+*/
+class XorWorld {
+    uint64_t A;
+    uint64_t B;
+    uint64_t diff;
+public : 
+    XorWorld(uint64_t A_p, uint64_t B_p) : A(min(A_p, B_p)), B(max(A_p,B_p)), diff(B - A + 1) {}
+    uint64_t GetXor (uint64_t A, uint64_t B) {
+        uint64_t ret;
+        for (uint64_t i = A; i <=B; i++) {
+            if (i == A) ret = i;
+            else ret ^= i;
+        }
+                        
 
+        return ret;
+    }
+
+    uint64_t Calculate() {
+        uint64_t ret;
+     if (diff < 5) {
+            ret = GetXor(A, B);
+        } else {
+                    
+            if ((A & 1) == 0) {
+                
+                diff = (diff & 3);
+                A = B - diff + 1;
+                ret = GetXor(A, B);
+               
+            } else if ((B & 1) == 1) {
+                diff = (diff & 3);
+                B = A + diff - 1;
+                ret = GetXor(A, B);
+
+            // Both are odd numbers
+            } else {
+                uint64_t A1 = A + 1;
+                diff--;
+                diff = (diff & 3);
+                A1 = B - diff + 1;
+                ret = A ^ GetXor(A1, B);
+            }
+        }
+        return ret;   
+    }
+
+};
 int main() {
     uint64_t A(in<uint64_t>()), B(in<uint64_t>());
-    uint64_t diff = B - A + 1;
-    uint64_t ret;
-//uint64_t exa = GetXor(A, B);
-
-    if (diff < 6) {
-        ret = GetXor(A, B);
-    } else {
-                
-        if ((A & 1) == 0) {
-            
-            diff = (diff & 3);
-            A = B - diff + 1;
-            ret = GetXor(A, B);
-           
-        } else if ((B & 1) == 1) {
-            diff = (diff & 3);
-            B = A + diff - 1;
-            ret = GetXor(A, B);
-
-        // Both are odd numbers
-        } else {
-            uint64_t A1 = A + 1;
-            diff--;
-            diff = (diff & 3);
-            A1 = B - diff + 1;
-            ret = A ^ GetXor(A1, B);
-        }
-    }   
+//    uint64_t diff = B - A + 1;
+    XorWorld world(A, B);
+   
 //     cout << exa << endl; 
-    cout << ret << endl;
+    cout << world.Calculate() << endl;
     return 0;
 }
 
