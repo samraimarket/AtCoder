9bc83f1aad86c41a14d4fafd80581817e24ff93e
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2019-03-11 20:57:19 +0900

    AC達成。次はリファクタリング

--- main.cpp
+++ main.cpp
@@ -13,16 +13,47 @@ T in() {
     return temp;
 }
 
+uint64_t GetXor (uint64_t A, uint64_t B) {
+    uint64_t ret;
+    for (uint64_t i = A; i <=B; i++) {
+        if (i == A) ret = i;
+        else ret ^= i;
+    }
+    return ret;
+}
 
 int main() {
     uint64_t A(in<uint64_t>()), B(in<uint64_t>());
-    uint64_t temp;
-    for (uint64_t i = A; i <=B; i++) {
-        if (i == A) temp = i;
-        else temp ^= i;
-        cout << std::bitset<4>(i) << ' ' << std::bitset<4>(temp) << endl;
-    }
-     
+    uint64_t diff = B - A + 1;
+    uint64_t ret;
+//uint64_t exa = GetXor(A, B);
+
+    if (diff < 6) {
+        ret = GetXor(A, B);
+    } else {
+                
+        if ((A & 1) == 0) {
+            
+            diff = (diff & 3);
+            A = B - diff + 1;
+            ret = GetXor(A, B);
+           
+        } else if ((B & 1) == 1) {
+            diff = (diff & 3);
+            B = A + diff - 1;
+            ret = GetXor(A, B);
+
+        // Both are odd numbers
+        } else {
+            uint64_t A1 = A + 1;
+            diff--;
+            diff = (diff & 3);
+            A1 = B - diff + 1;
+            ret = A ^ GetXor(A1, B);
+        }
+    }   
+//     cout << exa << endl; 
+    cout << ret << endl;
     return 0;
 }
 
