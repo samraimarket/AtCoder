bc31fdea46ef2374a49f67d9a56814bf4f8e2aa3
DESKTOP-BGDE0P6\takec <takechikazuma@gmail.com>
2019-03-11 20:10:18 +0900

    出力テスト

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <map>
 #include <algorithm>
+#include <bitset>
 using namespace std;
 template <typename T = int>
 T in() {
@@ -14,7 +15,14 @@ T in() {
 
 
 int main() {
-  
+    uint64_t A(in<uint64_t>()), B(in<uint64_t>());
+    uint64_t temp;
+    for (uint64_t i = A; i <=B; i++) {
+        if (i == A) temp = i;
+        else temp ^= i;
+        cout << std::bitset<4>(i) << ' ' << std::bitset<4>(temp) << endl;
+    }
+     
     return 0;
 }
 
